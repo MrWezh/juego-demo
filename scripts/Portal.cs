24051c06e1800b13d3ac89203e25027c3ac9df93
MrWezh <zhouwenkai16@gmail.com>
2025-12-17 10:39:16 +0100

    Nivel 2 transición a menu completado

--- scripts/Portal.cs
+++ scripts/Portal.cs
@@ -5,9 +5,17 @@ public partial class Portal : Area2D
 {
 	public void _on_body_entered(Node2D body)
 	{
-		 if (body is MainCharacter)
+		if (body is MainCharacter)
 		{
-			GetTree().CallDeferred("change_scene_to_file", "res://scenes/nivel_2.tscn");
+			string currentScene = GetTree().CurrentScene.SceneFilePath;
+			if (currentScene == "res://scenes/juegoDemo.tscn")
+			{
+				GetTree().CallDeferred("change_scene_to_file", "res://scenes/nivel_2.tscn");
+			}
+			else if (currentScene == "res://scenes/nivel_2.tscn")
+			{
+				GetTree().CallDeferred("change_scene_to_file", "res://scenes/menu.tscn");
+			}
 		}
 	}
 }
